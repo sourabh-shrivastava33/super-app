664b71cf679d9a57ea57c61061ed0fe2a1bb0118
sourabh shrivastava <sourabhshrivastava3011@gmail.com>
2023-11-26 23:59:48 +0530

    Add proxy news server

--- src/services/news-api.js
+++ src/services/news-api.js
@@ -1,14 +1,9 @@
 const newsBaseUrl = "https://newsapi.org/v2";
 export async function fetchNewsData({ signal } = {}) {
   try {
-    const data = await fetch(
-      `${newsBaseUrl}/everything?q=tech&apiKey=${
-        import.meta.env.VITE_NEWS_API_KEY
-      }`,
-      {
-        signal,
-      }
-    );
+    const data = await fetch("https://news-api-g1ij.onrender.com/news", {
+      signal,
+    });
     const resp = await data.json();
     return resp;
   } catch (error) {
